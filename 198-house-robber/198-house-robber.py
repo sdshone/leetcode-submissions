9df29e1ac5edbd3df6cffc53f9773e7783c4873d
sdshone <sdshone@gmail.com>
2022-07-30 22:40:06 +0530

    Time: 60 ms (23.48%), Space: 13.9 MB (66.03%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,16 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+# O(n) Space complexity        
+#         if not nums:
+#             return 0
+#         if len(nums) == 1:
+#             return nums[0]
+        
+#         dp = [0]*len(nums)
+#         dp[0] = nums[0]
+#         dp[1] = max(nums[0], nums[1])
+        
+#         for i in range(2, len(nums)):
+#             dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+#         print(dp)
+#         return dp[-1]
+        
         if not nums:
             return 0
         if len(nums) == 1:
             return nums[0]
         
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        
+        first = nums[0]
+        second = max(nums[0], nums[1])
+        curr = second
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
-        print(dp)
-        return dp[-1]
+            curr = max(first+nums[i], second)
+            first = second
+            second = curr
+        
+        return curr
