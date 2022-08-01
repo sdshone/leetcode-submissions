412699132e0f168047d29d576d7ea32e94230475
sdshone <sdshone@gmail.com>
2022-08-02 00:28:22 +0530

    Time: 9737 ms (5.02%), Space: 13.8 MB (92.79%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        
+        def dfs(i, r, c, sol):
+            # print(sol)
+            # print(i, r, c)
+            if i == len(word):
+                return True
+            if r<0 or r >= len(board) or c<0 or c >= len(board[0]) or (r,c) in sol:
+                return False
+
+            
+            if board[r][c] == word[i]:
+                sol.add((r,c))
+                res = (dfs(i+1, r+1,c, sol) or
+                dfs(i+1, r,c+1, sol) or
+                dfs(i+1, r-1,c, sol) or
+                dfs(i+1, r,c-1, sol))
+                sol.remove((r,c))
+                return res
+                
+            return False
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if dfs(0, r, c, set()):
+                    return True
+        return False
