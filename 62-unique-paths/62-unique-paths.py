09de52c2dc8c2d6c96ec191dcc737cc05b5a8e46
sdshone <sdshone@gmail.com>
2022-08-06 00:19:41 +0530

    Time: 104 ms (5.30%), Space: 14.1 MB (14.99%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        result = [[1]*n]*m
+        
+        if m==1 or n==1:
+            return 1
+        print(result)
+        for r in range(m-2,-1,-1):
+            for c in range(n-2, -1, -1):
+                print(r,c)
+                result[r][c]=result[r+1][c] + result[r][c+1]
+            print(result)
+        return result[0][0]
+            
