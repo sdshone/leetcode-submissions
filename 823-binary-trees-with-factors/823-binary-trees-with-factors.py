1ebd58dae77004d5c80e3085920090f2e2e1fea6
sdshone <sdshone@gmail.com>
2022-08-09 14:14:54 +0530

    Time: 2812 ms (10.60%), Space: 70.4 MB (5.05%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -3,10 +3,10 @@ class Solution:
         
         arr.sort()
         l=defaultdict(int)
-        for a in arr:
+        for i,a in enumerate(arr):
             temp = 1
-            for b in arr:
-                if b>a: break
+            for b in arr[:i+1]:
+                # if b>a: break
                 temp += l[b]*l[a/b]
             l[a]=temp
         return sum(l.values()) %(10**9+7)
