53b8b782b1e1b43e0c450a689c381a4d03d19852
sdshone <sdshone@gmail.com>
2022-08-09 14:53:26 +0530

    Time: 48 ms (60.86%), Space: 14 MB (22.89%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         res = 0
         for v in collections.Counter(s).values():
-            res += v//2*2
-            if res%2==0 and v%2==1:
+            if v%2==1:
                 res+=1
-        return res
+        print(res)
+        return len(s)- res+1 if res else len(s)
