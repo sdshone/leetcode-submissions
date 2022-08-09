98e52d5c0dfd802307764f65f5dd57e8a2474dcc
sdshone <sdshone@gmail.com>
2022-08-09 17:58:59 +0530

    Time: 2453 ms (5.03%), Space: 37.2 MB (15.41%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        cars = [[target - position[i], speed[i]] for i in range(len(speed))]
+        
+        cars.sort(key=lambda x: x[0], reverse=False)
+        
+        time = cars[0][0]/cars[0][1]
+        fleet = 1
+        for car in cars:
+            t = car[0]/car[1]
+            print(t, time)
+
+
+            if time <t:
+                fleet+=1
+            time = max(time, t)
+            
+        print(fleet)
+        return fleet
