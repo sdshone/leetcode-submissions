fa63734a23f3df01ee8f21e5447736d14ec9ab00
sdshone <sdshone@gmail.com>
2022-08-14 22:07:44 +0530

    Time: 155 ms (12.31%), Space: 14.2 MB (38.50%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -4,13 +4,11 @@ class Solution:
         
         src_color = image[sr][sc]
         done = set()
-        def dfs(r,c):
-            
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or image[r][c]!=src_color:
+        def dfs(r,c): 
+            if (r,c) in done or r < 0 or c < 0 or r >= ROWS or c >= COLS or image[r][c]!=src_color:
                 return
             
-            if (r,c) in done:
-                return
+
             done.add(tuple([r,c]))
             image[r][c] = color
             for row, col in [(r+1,c), (r-1,c), (r,c+1), (r,c-1)]:
