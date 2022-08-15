11cfe7d75b24afccbd826968c042db603fffdac7
sdshone <sdshone@gmail.com>
2022-08-15 15:50:00 +0530

    Time: 79 ms (27.72%), Space: 13.9 MB (75.20%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -7,23 +7,7 @@ class Solution:
         for i in range(ROWS):
             for j in range(COLS):
                 numWays[i][j] = int(not bool(numWays[i][j]))
-        
-        print(numWays)
-#         for i in range(ROWS):
-#             if numWays[i][-1] == 0:
-#                 break
-#         if i != ROWS:
-#             for z in range(i):
-#                 numWays[z][-1]=0
-            
-#         for i in range(COLS):
-#             if numWays[-1][i] == 0:
-#                 break
-#         if i != COLS:
-#             for z in range(i):
-#                 numWays[-1][z]=0
-            
-        print(numWays)
+
         if numWays[-1][-1] == 0: return 0
         
         for i in range(ROWS-1,-1,-1):
@@ -35,6 +19,6 @@ class Solution:
                         numWays[i][j] = numWays[i+1][j]
                     elif j+1 < COLS and i+1 < ROWS:
                         numWays[i][j] = numWays[i+1][j]+numWays[i][j+1]
-        print(numWays)
+
         return numWays[0][0]
         
