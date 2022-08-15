ed0ce909e99d02a3d5c1fba9a8a51b4e2b8f6856
sdshone <sdshone@gmail.com>
2022-08-15 13:19:25 +0530

    Time: 101 ms (40.16%), Space: 16.3 MB (6.90%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def __init__(self):
+        
+        self.paths = []
+        self.cache = {}
+        
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        if len(cost) <= 2:
+            return min(cost)
+        def dfs(i):
+            
+            if i < 2:
+                self.cache[i] = cost[i]
+                return cost[i]
+            if self.cache.get(i):
+                return self.cache.get(i)
+            x=dfs(i-1)
+            y=dfs(i-2)
+            
+            self.cache[i] = min(x,y)+ cost[i]
+            return self.cache[i]
+        
+        dfs(len(cost)-1)
+        print(self.cache)
+        return min(self.cache[len(cost)-1], self.cache.get(len(cost)-2, float('inf')))
