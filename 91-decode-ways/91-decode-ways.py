3755c5b5198068d8876bf2acb258c2c31e4c7054
sdshone <sdshone@gmail.com>
2022-08-15 22:03:12 +0530

    Time: 144 ms (5.38%), Space: 14.1 MB (30.61%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        mapping = {}
+        for i in range(1,27):
+            mapping[i] = chr(ord('A')+i-1)
+        print(mapping)
+
+        count = {len(s):1}
+        for i in range(len(s)-1,-1,-1):
+            if mapping.get(int(s[i])):
+                count[i] = count[i+1]
+                if i+1 < len(s) and mapping.get(int(s[i:i+2])):
+                    count[i]+=count[i+2]
+            else:
+                count[i]=0
+            print(count, s[i:i+2])
+                
+        return count[0]
+            
