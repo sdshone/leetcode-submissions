73a20785b036499d7338d10abd69541c430a5e83
sdshone <sdshone@gmail.com>
2022-08-25 16:42:03 +0530

    Time: 78 ms (67.01%), Space: 14.1 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        magzineCounter = collections.Counter(magazine)
+        ransomCounter = collections.Counter(ransomNote)
+        
+        for char in ransomNote:
+            if ransomCounter[char]>magzineCounter[char]:
+                return False
+        return True
