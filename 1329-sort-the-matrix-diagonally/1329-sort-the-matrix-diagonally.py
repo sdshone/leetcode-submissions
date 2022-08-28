582615ea5e2ee34d64a3cdc67e498a92868ec721
sdshone <sdshone@gmail.com>
2022-08-28 20:39:15 +0530

    Time: 106 ms (77.36%), Space: 14.4 MB (51.34%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        if not mat: return []
+        diag = defaultdict(list)
+        ROWS, COLS = len(mat), len(mat[0])
+        for i in range(ROWS):
+            for j in range(COLS):
+                diag[i-j].append(mat[i][j])
+        
+        for k in diag:
+            diag[k].sort(reverse=True)
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                mat[i][j] = diag[i-j].pop()
+        
+        return mat
+        
