724497fe3b80a574edc2f147c86ebf2de1565e30
sdshone <sdshone@gmail.com>
2022-09-07 21:50:01 +0530

    Time: 104 ms (22.05%), Space: 16.4 MB (30.02%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        
+        traversal = []
+        def dfs(node):
+            
+            if not node:
+                return
+            
+            
+            traversal.append(str(node.val))
+            if not node.left and node.right:
+                traversal.append('(')
+                traversal.append(')')
+            if node.left:
+                traversal.append('(')
+                dfs(node.left)
+                traversal.append(')')
+            if node.right:
+                traversal.append('(')
+                dfs(node.right)
+                traversal.append(')')
+            
+        dfs(root)
+        print(traversal)
+        traversal = ''.join(traversal)
+        print(traversal)
+        return traversal
