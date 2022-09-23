b8328e23f2f5fbef27142b49d632189bd4b33eba
sdshone <sdshone@gmail.com>
2022-09-24 01:03:13 +0530

    Time: 452 ms (5.14%), Space: 29.7 MB (50.00%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -8,21 +8,56 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
         
-        nodes = set()
-        while headA or headB:
-            # print(headA.val)
-            # print(headB in nodes)
-            if headA == headB:
-                return headA
-            if headA in nodes:
-                return headA
-            elif headB in nodes:
-                return headB
-            if headA:
-                nodes.add(headA)
-                headA = headA.next
-            if headB:
-                nodes.add(headB)
-                headB = headB.next
+        # nodes = set()
+        # while headA or headB:
+        #     # print(headA.val)
+        #     # print(headB in nodes)
+        #     if headA == headB:
+        #         return headA
+        #     if headA in nodes:
+        #         return headA
+        #     elif headB in nodes:
+        #         return headB
+        #     if headA:
+        #         nodes.add(headA)
+        #         headA = headA.next
+        #     if headB:
+        #         nodes.add(headB)
+        #         headB = headB.next
+        
+        refA = headA
+        refB = headB
+        count = 0
+        while refA != refB:
+            refA = refA.next
+            refB = refB.next  
+            if refA is None:
+                count+=1
+                if count == 2:
+                    return
+                refA = headB
+            if refB is None:
+                refB = headA
+
+        return refA
+                
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
             
