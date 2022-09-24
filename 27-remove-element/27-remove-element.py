3bd4541933b851cb283ea290e1790496bfc9b2c2
sdshone <sdshone@gmail.com>
2022-09-24 15:12:27 +0530

    Time: 71 ms (14.28%), Space: 13.9 MB (14.38%) - LeetHub

---
+++ 27-remove-element/27-remove-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        
+        nums.sort()
+        i = 0
+        N = len(nums)
+        while i <  N:
+            if nums[i] == val:
+                curr = start = i
+                while curr < N and nums[curr] == val:
+                    curr +=1
+                if curr == N: return i
+                end = curr - i
+                while i < curr and curr < N:
+                    nums[i] = nums[curr]
+                    i+=1
+                    curr+=1
+                return i
+            i +=1
+        return N
+                
