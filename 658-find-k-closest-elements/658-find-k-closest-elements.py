0c9d89105de1f2cfd4455af097a2392bc5a989eb
sdshone <sdshone@gmail.com>
2022-09-29 19:08:35 +0530

    Time: 983 ms (5.72%), Space: 16.2 MB (13.93%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+        d = defaultdict(list)
+        for n in arr:
+            d[(abs(n-x))].append(n)
+        res = []
+        count = 0
+        for key in sorted(d.keys()):
+            
+            res.extend(d[key])
+            count += len(d[key])
+            if count > k:
+                break
+        return sorted(res[:k])
