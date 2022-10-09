da1919388b6754c57ec776b23e5fa70fd54e0e2d
sdshone <sdshone@gmail.com>
2022-10-09 12:23:54 +0530

    Time: 469 ms (8.93%), Space: 23.8 MB (46.46%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        
+        
+        def dfs(nums):
+            N=len(nums)
+            if N==1:
+                return nums[0]
+            
+            mid = N//2
+            print(N, mid)
+            print(nums)
+            if nums[mid] == nums[mid-1]:
+                if mid%2!=0:
+                    x=dfs(nums[mid+1:])
+                else:
+                    x=dfs(nums[:mid-1])
+            elif(nums[mid] == nums[mid+1]):
+                if mid%2==0:
+                    x=dfs(nums[mid+2:])
+                else:
+                    x=dfs(nums[:mid])
+            else: x= nums[mid]
+            
+            return x
+    
+        return dfs(nums)
