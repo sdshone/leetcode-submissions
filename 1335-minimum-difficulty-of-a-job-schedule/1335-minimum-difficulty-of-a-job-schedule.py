00440fc1b626022c0156ea439bfe0df1f24a7aa8
sdshone <sdshone@gmail.com>
2022-10-16 15:09:34 +0530

    Time: 3129 ms (21.68%), Space: 15.1 MB (36.89%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -4,8 +4,10 @@ class Solution:
         J = len(jobDifficulty)
         if d>J:
             return -1
-        @lru_cache(None)
+        memo={}
+        # @lru_cache(None)
         def dfs(i,d):
+            if (i,d) in memo: return memo[(i,d)]
             if d==0:
                 if i==J: return 0
                 else: return float('inf')
@@ -17,5 +19,6 @@ class Solution:
             for idx in range(i, J):
                 count_max = max(count_max, jobDifficulty[idx])
                 ans = min(ans, count_max+ dfs(idx+1, d-1))
+            memo[(i,d)] = ans
             return ans
         return dfs(0, d)
