9f1cbde557fe06c2bbbd08dd9db64fabf0528f4a
sdshone <sdshone@gmail.com>
2022-10-18 20:49:34 +0530

    Time: 164 ms (5.23%), Space: 13.9 MB (86.43%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s='1'
+        def countsay(s):
+            res=''
+            idx=0
+            while idx<len(s):
+                char=s[idx]
+                count = 0
+                while idx<len(s) and s[idx]==char:
+                    idx+=1
+                    count+=1
+        
+                res+=f'{count}{char}'
+                count=0
+            return res
+        for i in range(n-1):
+            s = countsay(s)
+            # print(s)
+        
+        return s
