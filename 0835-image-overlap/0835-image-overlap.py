d5ebe95ef5bd02eb3ef85661197a1171cd2e2ea4
sdshone <sdshone@gmail.com>
2022-10-27 20:36:31 +0530

    Time: 389 ms (94.15%), Space: 14.7 MB (43.86%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -2,14 +2,14 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         
         N = len(img1)
-        a ,b = [], []
+        a ,b = set(), set()
         d = defaultdict(int)
         for row in range(N):
             for col in range(N):
                 if img1[row][col]:
-                    a.append((row, col))
+                    a.add((row, col))
                 if img2[row][col]:
-                    b.append((row, col))
+                    b.add((row, col))
         
         for ra, ca in a:
             for rb, cb in b:
