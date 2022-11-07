fd34437afc92afadd8122d8ff4ad85c25f304545
sdshone <sdshone@gmail.com>
2022-11-08 00:46:17 +0530

    Time: 91 ms (45.04%), Space: 14.6 MB (51.46%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -14,23 +14,34 @@ class Solution:
 #         return max(buy, cooldown, sell)
         
 
+#         P = len(prices)
+#         dp = {}
+#         def dfs(i, sell):
+
+#             if i >= P:
+#                 return 0
+#             if (i,sell) in dp:
+#                 return dp[(i,sell)]
+
+#             if sell is not None:
+#                 t = prices[i]-sell
+#                 dp[(i,sell)] = max(t+dfs(i+2, None), dfs(i+1, sell))
+#             else:
+#                 #buy
+#                 dp[(i,sell)] = max(dfs(i+1, prices[i]),  dfs(i+1, sell))
+#             return dp[(i,sell)]  
+
+#         return dfs(0, None)
+    
         P = len(prices)
-        dp = {}
-        def dfs(i, sell):
-
-            if i >= P:
-                return 0
-            if (i,sell) in dp:
-                return dp[(i,sell)]
-            # no buy no sell
-            
-            if sell is not None:
-                t = prices[i]-sell
-                dp[(i,sell)] = max(t+dfs(i+2, None), dfs(i+1, sell))
-            else:
-                #buy
-                dp[(i,sell)] = max(dfs(i+1, prices[i]),  dfs(i+1, sell))
-            return dp[(i,sell)]
+        dp = [[0 for _ in range(2)] for _ in range(P+2)]
         
-
-        return dfs(0, None)
+        for i in range(P-1, -1, -1):
+            
+            dp[i][1]  = max(-prices[i]+dp[i+1][0], dp[i+1][1])
+            dp[i][0]  = max(prices[i]+dp[i+2][1], dp[i+1][0])
+        
+        return dp[0][1]
+    
+    
+    
