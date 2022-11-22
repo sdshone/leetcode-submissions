17640785def849e63c68ea79475a07b48fd34248
sdshone <sdshone@gmail.com>
2022-11-22 19:45:41 +0530

    Time: 394 ms (76.48%), Space: 14 MB (71.48%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,21 +1,51 @@
 class Solution:
+
+    dp = [0]
+
     def numSquares(self, n: int) -> int:
+
+        dp = self.dp
+
+        perfectSq = [pow(i,2) for i in range(1, int(sqrt(n))+1)]
+
+        while len(dp) < n+1:
+
+            dpI = inf
+
+            for ps in perfectSq:
+                if len(dp)<ps: break
+                dpI = min(dpI,1+dp[-ps])
+            
+            dp.append(dpI)
         
-        
-        
-        dp = [100 for _ in range(n+1)]
-        dp[0] = 0
-        
-        r = 1
-        sq = r*r
-        
-        while sq <= n:
-            for i in range(sq, n+1):
-                dp[i] = min(dp[i], dp[i-sq]+1)
-            r += 1
-            sq = r*r
-        # print(max(dp))
         return dp[n]
+
+
+
+
+
+
+
+
+
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+        
+        
+        
+#         dp = [100 for _ in range(n+1)]
+#         dp[0] = 0
+        
+#         r = 1
+#         sq = r*r
+        
+#         while sq <= n:
+#             for i in range(sq, n+1):
+#                 dp[i] = min(dp[i], dp[i-sq]+1)
+#             r += 1
+#             sq = r*r
+#         # print(max(dp))
+#         return dp[-1]
 
 #         dp = {}
 #         def dfs(n):
