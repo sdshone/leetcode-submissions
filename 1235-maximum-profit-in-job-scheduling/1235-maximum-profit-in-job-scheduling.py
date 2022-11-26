dcfd1e1b33c16a70222adac99752bfa6d2a8394c
sdshone <sdshone@gmail.com>
2022-11-26 20:46:56 +0530

    Time: 2204 ms (10.11%), Space: 110.5 MB (5.12%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -17,7 +17,7 @@ class Solution:
                     r = m
             # l = l if l< len(nums) else len(nums)-1
             # print(l,r, target)
-            if l >= len(nums): return None
+            # if l >= len(nums): return None
             # return nums[l-1][0] 
             return l
         
@@ -26,7 +26,7 @@ class Solution:
             if i >= len(jobs) : return 0
             k = binarySearch(jobs, jobs[i][1])
             # k = bisect_left(jobs, jobs[i][1], key = lambda x: x[0])
-            x = dfs(k) if k is not None else 0
-            return max(dfs(i+1), jobs[i][2] + x)
+            # x = dfs(k) if k is not None else 0
+            return max(dfs(i+1), jobs[i][2] + dfs(k))
     
         return dfs(0)
