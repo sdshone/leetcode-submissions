eefd79f09be84a4233ee29b742424649d524221e
sdshone <sdshone@gmail.com>
2022-11-26 20:42:29 +0530

    Time: 980 ms (72.33%), Space: 47.4 MB (21.62%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -18,13 +18,14 @@ class Solution:
             # l = l if l< len(nums) else len(nums)-1
             # print(l,r, target)
             if l >= len(nums): return None
-            return nums[l-1][0] 
+            # return nums[l-1][0] 
+            return l
         
         @lru_cache(None)
         def dfs(i):
             if i >= len(jobs) : return 0
-            # k = binarySearch(jobs, jobs[i][1])
-            k = bisect_left(jobs, jobs[i][1], key = lambda x: x[0])
+            k = binarySearch(jobs, jobs[i][1])
+            # k = bisect_left(jobs, jobs[i][1], key = lambda x: x[0])
             x = dfs(k) if k is not None else 0
             return max(dfs(i+1), jobs[i][2] + x)
     
