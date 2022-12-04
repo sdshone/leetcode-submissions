a0b7ce17612f82ff4087da7d342a2a5323c8b39b
sdshone <sdshone@gmail.com>
2022-12-04 08:14:02 +0530

    Time: 1359 ms (79.14%), Space: 31 MB (5.46%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,20 +1,21 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         
-        prefix = [0]*len(nums)
-        suffix = [0]*(len(nums)+1)
+        N = len(nums)
+        prefix = [0]*N
+        suffix = [0]*(N+1)
         
         for idx,num in enumerate(nums):
             prefix[idx] = num+ prefix[idx-1]
-        for idx in range(len(nums)-1, -1, -1):
+        for idx in range(N-1, -1, -1):
             suffix[idx] = nums[idx] + suffix[idx+1]
         res = float('inf')
         res_index = 0
 
-        for i in range(len(nums)-1,-1,-1):
+        for i in range(N-1,-1,-1):
 
-            if (len(nums) - i-1):
-                v = abs(int(prefix[i]/(i+1)) - int(suffix[i+1]/(len(nums) - i-1)))
+            if (N - i-1):
+                v = abs(int(prefix[i]/(i+1)) - int(suffix[i+1]/(N - i-1)))
             else:
                 v = int(prefix[i]/(i+1))
             res = min(res, v)
